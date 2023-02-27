5d4fb22ec710cd603266894c123f7a5ffaf25ded
KamahGit <bruceirungupayee@gmail.com>
2023-02-27 17:43:46 +0300

    minor error

--- convert_audio.ps1
+++ convert_audio.ps1
@@ -6,4 +6,4 @@ $file_name = [System.IO.Path]::GetFileNameWithoutExtension($filePath)
 $new_file_name = "$file_name"+"_conv"+"$file_extension"
 Write-Host $new_file_name
 
-ffmpeg -v debug -i $file_path -c:a libmp3lame -b:a 64k -ac 2 -ar 44100 -vn $new_file_name
+ffmpeg -v debug -i $filePath -c:a libmp3lame -b:a 64k -ac 2 -ar 44100 -vn $new_file_name
