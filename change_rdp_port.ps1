918ad3e949ad9a19c5add3b8a87c35fb992c20bb
Bruce Kamau Irungu <bruceirungupayee@gmail.com>
2023-11-25 22:45:05 +0300

    Update change_rdp_port.ps1

--- change_rdp_port.ps1
+++ change_rdp_port.ps1
@@ -3,6 +3,7 @@ $customRdpPort = 5125
 
 # Add a new firewall rule for the custom RDP port
 New-NetFirewallRule -DisplayName "CustomRDP" -Direction Inbound -Protocol TCP -LocalPort $customRdpPort -Action Allow
+New-NetFirewallRule -DisplayName "CustomRDP" -Direction Inbound -Protocol UDP -LocalPort $customRdpPort -Action Allow
 
 # Change the RDP port in the registry
 $registryPath = "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp"
@@ -10,4 +11,4 @@ $registryName = "PortNumber"
 Set-ItemProperty -Path $registryPath -Name $registryName -Value $customRdpPort
 
 # Restart the RDP service
-Restart-Service -Name TermService
+Restart-Service -Force TermService
